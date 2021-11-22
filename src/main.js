696fb123e418e22ace4987ab388cefea592014cb
mzawadi <mwaranguallan345@gmail.com>
2021-11-22 16:08:58 +0300

    update

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import marketplaceAbi from "../contract/marketplace.abi.json"
 import erc20Abi from "../contract/erc20.abi.json"
 
 const ERC20_DECIMALS = 18
-const MPContractAddress = "0x9F1fE1e97d16beaDd4b1a1E89cBCB80635cfA12B"
-const cUSDContractAddress = "0x68DB12FFf61176921407EE87bfbDaE4252fC9D76"
+const MPContractAddress = "0xE1ea345FEeA9401C0f3E7593092436D4703ACB8a"
+const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1"
 
 let kit
 let contract
